3c2beb42f87298031a2a60147b973964ab4ca2fb
tecsxpert <dev@tecsxpert.xyz>
2026-02-16 11:56:52 +0530

    Added Variables

---
+++ variables.py
@@ -0,0 +1,34 @@
+#Allowed
+myage=35
+my_age=35
+_my_age=35
+myAge=35
+MYAGE=35
+myage1=35
+
+#Wrong/Illegal
+#1myage=35
+#my-var=35
+#my var=35
+
+#Assignments
+a,b,c = "Dennis","Manjunath","Sneha"
+print(a)
+print(b)
+print(c)
+
+d=e=f = "Dennis"
+print(d)
+print(e)
+print(f)
+
+name="dennis"
+age = 35
+height = 5.6
+is_married=True
+
+print("Hi I am ",name)
+print("I am ",age," Years old")
+print("My Height is ",height," feet")
+
+
