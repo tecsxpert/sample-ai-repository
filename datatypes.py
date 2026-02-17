72ec9975ab5d97c962018463c59bbe77b719bcb2
tecsxpert <dev@tecsxpert.xyz>
2026-02-17 11:44:11 +0530

    Added Datatypes related Files and KT Items

--- datatypes.py
+++ datatypes.py
@@ -1,39 +1,54 @@
 #String
-message="Hey Bro Wassup"
-name='Python'
+#message="Hey Bro Wassup"
+#name='Python'
 
-poem="""
-Roses are red,
-Violets are blue,
-Python is awesome,
-Well so are you!
-"""
+#poem="""
+#Roses are red,
+#Violets are blue,
+#Python is awesome,
+#Well so are you!
+#"""
 
 #string operations
-first_name="jacob"
-last_name="dennis"
+#first_name="jacob"
+#last_name="dennis"
 
 #Concatenate
-fullname=first_name+ " " + last_name
-print(fullname.upper())
+#fullname=first_name+ " " + last_name
+#print(fullname.upper())
 
 #Repeatetive characters
-line = "*" * len(fullname)
-print(line)
+#line = "*" * len(fullname)
+#print(line)
 
-print(fullname[6])
+#print(fullname[6])
 #Integer
-a=10
-b=20
+#a=10
+#b=20
 
-print("Addition ",a+b)
-print("Subtraction ",a-b)
-print("Multiplication ",a*b)
-print("Division ",a/b)
-print("Integer Division ",a//b)
-print(" Modulus ",a%b)
-print("Exponent ",a**b)
+#print("Addition ",a+b)
+#print("Subtraction ",a-b)
+#print("Multiplication ",a*b)
+#print("Division ",a/b)
+#print("Integer Division ",a//b)
+#print(" Modulus ",a%b)
+#print("Exponent ",a**b)
 
 #Floats
-#Booleans
+price=20.99
+PI=3.14285
+temp=99.8
 
+total=price*1.5
+print(total)
+
+print(0.1+0.345)
+print(round(0.1+0.345,5))
+
+#Booleans
+is_raining=False
+is_holiday=False
+age=10
+
+is_adult=age >= 18
+#is_alult=False
