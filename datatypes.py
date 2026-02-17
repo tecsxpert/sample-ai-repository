db04e008eade410967b2f5c4520f331271b81d41
tecsxpert <dev@tecsxpert.xyz>
2026-02-17 11:24:06 +0530

    Added New Datatypes

---
+++ datatypes.py
@@ -0,0 +1,39 @@
+#String
+message="Hey Bro Wassup"
+name='Python'
+
+poem="""
+Roses are red,
+Violets are blue,
+Python is awesome,
+Well so are you!
+"""
+
+#string operations
+first_name="jacob"
+last_name="dennis"
+
+#Concatenate
+fullname=first_name+ " " + last_name
+print(fullname.upper())
+
+#Repeatetive characters
+line = "*" * len(fullname)
+print(line)
+
+print(fullname[6])
+#Integer
+a=10
+b=20
+
+print("Addition ",a+b)
+print("Subtraction ",a-b)
+print("Multiplication ",a*b)
+print("Division ",a/b)
+print("Integer Division ",a//b)
+print(" Modulus ",a%b)
+print("Exponent ",a**b)
+
+#Floats
+#Booleans
+
