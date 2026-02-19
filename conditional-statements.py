f856de8f2eba5a968c5089fd549b37c284e2e4b4
tecsxpert <dev@tecsxpert.xyz>
2026-02-19 11:52:53 +0530

    Added Conditional Statements

---
+++ conditional-statements.py
@@ -0,0 +1,82 @@
+#name_str = input("Hi what is your name?")
+#age=input("Hi "+name_str+", what is your age?")
+
+
+#IF Else Condition
+# if password=="secret@123":
+#     print("Access Granted")
+# else:
+#     print("Access Denied")
+# if int(age)  >= 18:
+#     print("You are a Adult")
+#     print("You can vote!")
+# else:
+#     print("You are a bacha")
+#     print("Go have some ice cream")
+
+#IF else if 
+# score = 85
+
+# if score >= 90:
+#     grade='A'
+# elif score >=80:
+#     grade = 'B'
+# elif score >=70:
+#     grade = 'C'
+# elif score >=60:
+#     grade='D'
+# else:
+#     grade='F'
+
+# print("Your Grade is :",grade)
+
+
+#Nested If Else
+# age=25
+# has_license=False
+
+# if age>=18:
+#     print("You are old enough to drive")
+
+#     if has_license:
+#         print("You can Drive!")
+#     else:
+#         print("But you need a License")
+# else:
+#     print("Too Young to drive")
+
+#If else using "AND"
+
+# age=20
+# has_ticket=False
+# if age >= 18 and has_ticket:
+#     print("You can enter the concert")
+# else:
+#     print("Cannot Enter")
+
+#If else using "OR"
+# day="monday"
+
+# if day =="saturday" or day=="sunday":
+#     print("It's the Weekend! OPen the book")
+# else:
+#     print("Its a weekday")
+
+# is_sunny=True
+
+# if not is_sunny:
+#     print("its Sunny")
+# else:
+#     print("Its cloudy")
+
+#Ternary Operator
+age=21
+
+# if age>=18:
+#     status="adult"
+# else:
+#     status="minor"
+
+status = "adult" if age >= 18 else "minor"
+print(status)
+
