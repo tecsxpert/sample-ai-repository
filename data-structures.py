4487a09f5ad9ff780c910eb9905b34cf04a5830d
tecsxpert <dev@tecsxpert.xyz>
2026-02-24 11:23:17 +0530

    Added Lists

---
+++ data-structures.py
@@ -0,0 +1,49 @@
+#Lists
+students=["Dennis","Manjunath","Kavya","Sneha"]
+numbers=[1,2,3,4,4,5]
+mixed=[1,"Dennis",25.4,True]
+
+#print(students)
+#students[1]="Arun"
+#students.pop()
+# print(students)
+# #Length
+# print("Lenght of students listy is ",len(students))
+# #sum,min,mx
+# print("Sum of all numbers in Numbers list is ",sum(numbers))
+# print("min number in the numbers list is ",min(numbers))
+# print("max number in the numbers list is ",max(numbers))
+# #count
+# print("Total number of occurences of number 4 is ",numbers.count(4))
+# #find index
+# print("The index of the List Item Manjunath is ",students.index("Manjunath"))
+# #sort
+# students.sort()
+# print(students)
+# #revese
+# numbers.reverse()
+# print(numbers)
+# #Check Membership
+# print("Manjunath"  in students)
+# for name in students:
+#     print(name)
+
+# print(range(len(students)))
+
+# for i in range(len(students)):
+#     print(f"{i}: {students[i]}")
+
+# print(enumerate(students))
+# for k,v in enumerate(students):
+#     print(f"{k}: {v}")
+
+#squares = []
+# for i in range(1,6):
+#     squares.append(i ** 2)
+
+squares = [i ** 2 for i in range(1,6)]   
+print(squares)
+
+#Tuples
+#Dictionaries
+#Sets
